2f124b94539c4fee0aa17a1bd073896314cb780a
Sujan Maharjan <sujanmh@noveltytechnology.com>
2024-08-15 03:19:22 +0545

    Changed Theme, Added Internet Permission, Added Option Menu, Added Volley package, Implemented Volley

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation("com.github.yukuku:ambilwarna:2.0.1")
+    implementation("com.android.volley:volley:1.2.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
