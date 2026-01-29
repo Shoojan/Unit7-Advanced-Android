685732a5abf6c18134a10e9b035934ed1a423164
SujanMh <sujanmh@noveltytechnology.com>
2026-01-29 23:22:18 +0545

    Setup the base for SQLHelper

---
+++ app/src/main/java/com/example/unit7_sqlite/DatabaseConfig.java
@@ -0,0 +1,15 @@
+package com.example.unit7_sqlite;
+
+public class DatabaseConfig {
+    // Database
+    private static final String DATABASE_NAME = "<DB_NAME>";
+    private static final int DATABASE_VERSION = 1;
+
+    // Table Name
+    private static final String TABLE_NAME = "<TABLE_NAME>";
+
+    // Table Columns
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_COLOR = "color";
+}
