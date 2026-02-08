ffe12afd21fbcfbe267d813aac9e5e172792b242
SujanMh <sujanmh@noveltytechnology.com>
2026-02-08 08:34:06 +0545

    WIP

--- app/src/main/java/com/example/unit7_sqlite/DatabaseConfig.java
+++ app/src/main/java/com/example/unit7_sqlite/DatabaseConfig.java
@@ -2,14 +2,14 @@ package com.example.unit7_sqlite;
 
 public class DatabaseConfig {
     // Database
-    private static final String DATABASE_NAME = "<DB_NAME>";
-    private static final int DATABASE_VERSION = 1;
+     static final String DATABASE_NAME = "subjectDb";
+     static final int DATABASE_VERSION = 1;
 
     // Table Name
-    private static final String TABLE_NAME = "<TABLE_NAME>";
+     static final String TABLE_NAME = "subjectTable";
 
     // Table Columns
-    private static final String COLUMN_ID = "id";
-    private static final String COLUMN_NAME = "name";
-    private static final String COLUMN_COLOR = "color";
+     static final String COLUMN_ID = "id";
+     static final String COLUMN_NAME = "name";
+     static final String COLUMN_COLOR = "color";
 }
--- app/src/main/java/com/example/unit7_sqlite/SQLiteHelper.java
+++ app/src/main/java/com/example/unit7_sqlite/SQLiteHelper.java
@@ -1,6 +1,70 @@
 package com.example.unit7_sqlite;
 
-public class SQLiteHelper {
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.ContactsContract;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SQLiteHelper extends SQLiteOpenHelper {
+
+    public SQLiteHelper(Context context) {
+        super(context, DatabaseConfig.DATABASE_NAME, null, DatabaseConfig.DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String CREATE_TABLE = "CREATE TABLE " + DatabaseConfig.TABLE_NAME + " ("
+                + DatabaseConfig.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + DatabaseConfig.TABLE_NAME + " TEXT,"
+                + DatabaseConfig.COLUMN_COLOR + " INTEGER"
+                + ");";
+
+        sqLiteDatabase.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + DatabaseConfig.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+
+    public void addSubject(String name, int color){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DatabaseConfig.COLUMN_NAME, name);
+        contentValues.put(DatabaseConfig.COLUMN_COLOR, color);
+
+        db.insert(DatabaseConfig.TABLE_NAME, null, contentValues);
+        db.close();
+    }
+
+    public List<Subject> getAllSubjects(){
+        List<Subject> subjectList = new ArrayList<>();
+
+        String SELECT_QUERY = "SELECT * FROM "+ DatabaseConfig.TABLE_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(SELECT_QUERY, null);
+
+        if(cursor.moveToFirst()){
+            do{
+                Subject subject = new Subject(
+                        cursor.getInt(cursor.getColumnIndex(DatabaseConfig.COLUMN_ID)),
+                        cursor.getString(cursor.getColumnIndex(DatabaseConfig.COLUMN_NAME)),
+                        cursor.getInt(cursor.getColumnIndex(DatabaseConfig.COLUMN_COLOR))
+                );
+                subjectList.add(subject);
+            }while (cursor.moveToNext());
+        }
+
+        return subjectList;
+    }
 
 //      STEPS:
 //      ------
